9cf7d26629a7145611c3afce270e2001d85acde2
Fergus Molloy <fergus@molloy.xyz>
2023-02-18 18:46:56 +0000

    basic system working

--- flake.nix
+++ flake.nix
@@ -10,31 +10,14 @@
   };
   outputs = { self, nixpkgs, home-manager }:
     let 
-      system = "x86_64-linux";
       user = "fergus";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
       lib = nixpkgs.lib;
     in {
-      nixosConfigurations = {
-  kirin = lib.nixosSystem {
-    inherit system;
-    specialArgs = { inherit user; };
-    modules = [ 
-      ./hosts/configuration.nix 
-      ./hosts/kirin/hardward-configuration.nix
-      home-manager.nixosModules.home-manager {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        extraSpecialArgs = { inherit user; };
-        home-manager.users.${user} = {
-          imports = [ ./hosts/home.nix ];
-        };
-      }
-      ];
-  };
-      };
+      nixosConfigurations = (
+        import ./hosts {
+          inherit (nixpkgs) lib;
+          inherit nixpkgs home-manager user;
+        }
+      );
     };
 }
--- hosts/default.nix
+++ hosts/default.nix
@@ -1,19 +1,21 @@
-{system, lib, home-manager, user, ... }:
-{
+{ lib, home-manager, user, ... }:
+let
+  system = "x86_64";
+in {
   kirin = lib.nixosSystem {
     inherit system;
     specialArgs = { inherit user; };
     modules = [ 
       ./configuration.nix 
-      ./kirin/hardward-configuration.nix
+      ./kirin/hardware-configuration.nix
       home-manager.nixosModules.home-manager {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
-        extraSpecialArgs = { inherit user; };
+        home-manager.extraSpecialArgs = { inherit user; };
         home-manager.users.${user} = {
           imports = [ ./home.nix ];
         };
       }
       ];
-  }
+  };
 }
