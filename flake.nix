45bda0f6ad844e4dbb519affd0d7498b5044774b
Fergus Molloy <fergus@molloy.xyz>
2023-07-23 10:11:29 +0100

    reduce packages switch to stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My personal flake for all my devices";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
