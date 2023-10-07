f9ffddc3accd1e41d9644a4c081e28fef11f8725
Fergus Molloy <fergus@molloy.xyz>
2023-10-07 17:49:23 +0100

    setup odogaron

--- hosts/odogaron/home.nix
+++ hosts/odogaron/home.nix
@@ -7,5 +7,12 @@
     ../../modules/tmux.nix
   ];
   home.file.".background-image".source = ../../modules/wall-1440;
+  home.file.".screen-setup" = {
+    executable = true;
+    text = ''
+      #!/usr/bin/env bash
+      xrandr --auto --output HDMI-1 --mode 1920x1080 --same-as eDP-1
+    '';
+  };
   programs.feh.enable = true;
 }
