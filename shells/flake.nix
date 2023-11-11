f96695995e20d8e7543515c8d56f8b04f0858a06
Fergus Molloy <fergus@molloy.xyz>
2023-11-11 17:22:18 +0000

    update shell flake

--- shells/flake.nix
+++ shells/flake.nix
@@ -20,7 +20,7 @@
           lua = mkShellNoCC { buildInputs = [ lua-language-server stylua ]; };
           astro = mkShellNoCC { buildInputs = [ nodejs_20 nodePackages."vscode-langservers-extracted" nodePackages."@astrojs/language-server" nodePackages."typescript-language-server" nodePackages."typescript" ]; };
           node = mkShellNoCC { buildInputs = [ nodejs_20 nodePackages."vscode-langservers-extracted" ]; };
-          elixir = mkShellNoCC { buildInputs = [ elixir libnotify ]; };
+          elixir = mkShellNoCC { buildInputs = [ elixir libnotify inotify-tools ]; };
           ocaml = mkShell {
             nativeBuildInputs = with ocamlPackages; [ ocaml findlib dune_3 ocaml-lsp utop ocamlformat odoc ];
             buildInputs = with ocamlPackages; [ ocamlgraph ocamlPackages.core ocamlPackages.core_unix ];
