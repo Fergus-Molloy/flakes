0ff36018b02774ad754454c0b108e476c8a557b3
Fergus Molloy <fergus@molloy.xyz>
2023-11-13 09:27:52 +0000

    add tmuxinator

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -5,9 +5,10 @@
     baseIndex = 1;
     clock24 = true;
     terminal = "tmux-256color";
-    plugins = with pkgs; [ tmuxPlugins.resurrect ];
-    # extraConfig = ''
-    #   set-option -ga terminal-overrides ",xterm-256color:Tc"
-    # '';
+    plugins = with pkgs; [ ];
+    extraConfig = ''
+      set -g mouse on
+    '';
+    #set-option -ga terminal-overrides ",xterm-256color:Tc"
   };
 }
