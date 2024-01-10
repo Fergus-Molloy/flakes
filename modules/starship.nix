0cb22e43b67913ae1b287ed7699ae55cfa63527a
Fergus Molloy <fergus@molloy.xyz>
2024-01-10 12:57:08 +0000

    add rose-pine terminal theme

--- modules/starship.nix
+++ modules/starship.nix
@@ -5,43 +5,43 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
-      format = "[](#d65d0e)$username$hostname[](bg:#98971a fg:#d65d0e)$directory[](fg:#98971a bg:#689d6a)$git_branch$git_status[](fg:#689d6a bg:#458588)$nodejs$rust[](fg:#458588 bg:#b16286)$cmd_duration[](#b16286)$line_break $character";
+      format = "[](#eb6f92)$username$hostname[](bg:#3e8fb0 fg:#eb6f92)$directory[](fg:#3e8fb0 bg:#ea9a97)$git_branch$git_status[](fg:#ea9a97 bg:#9ccfd8)$nodejs$rust[](fg:#9ccfd8 bg:#c4a7e7)$cmd_duration[](#c4a7e7)$line_break $character";
       username = {
         show_always = true;
-        style_user = "bg:#d65d0e";
-        style_root = "bg:#d65d0e";
+        style_user = "bg:#eb6f92";
+        style_root = "bg:#eb6f92";
         format = "[$user]($style)";
       };
       hostname = {
         ssh_only = false;
-        style = "bg:#d65d0e";
+        style = "bg:#eb6f92";
         format = "[@$hostname ]($style)";
       };
       directory = {
-        style = "bg:#98971a";
+        style = "bg:#3e8fb0";
         format = "[ $path ]($style)";
         truncation_length = 3;
         truncation_symbol = ".../";
         truncate_to_repo = true;
       };
       git_branch = {
-        style = "bg:#689d6a";
+        style = "bg:#ea9a97";
         format = "[ $symbol$branch ]($style)";
       };
       git_status = {
-        style = "bg:#689d6a";
+        style = "bg:#ea9a97";
         format = "[$all_status$ahead_behind ]($style)";
       };
       nodejs = {
-        style = "bg:#458588";
+        style = "bg:#9ccfd8";
         format = "[ $symbol ($version)]($style)";
       };
       rust = {
-        style = "bg:#458588";
+        style = "bg:#9ccfd8";
         format = "[ $symbol\($toolchain\)]($style)";
       };
       cmd_duration = {
-        style = "bg:#b16286";
+        style = "bg:#c4a7e7";
         format = "[ $duration]($style)";
       };
     };
