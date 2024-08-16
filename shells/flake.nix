f213c0ca79c5ded7842e2c1afc70720eff099693
Fergus Molloy <fergus@molloy.xyz>
2024-08-16 09:41:53 +0100

    use melange as kitty theme

--- shells/flake.nix
+++ shells/flake.nix
@@ -25,7 +25,7 @@
               solid = mkShellNoCC { buildInputs = [ nodejs_18 nodePackages."prettier" nodePackages."vscode-langservers-extracted" nodePackages."typescript-language-server" nodePackages."typescript" ]; };
               elixir = mkShellNoCC { buildInputs = [ elixir elixir-ls libnotify inotify-tools ]; };
               erlang = mkShellNoCC { buildInputs = [ beam.packages.erlang_24.erlang erlang-ls rebar3 ]; };
-              go = mkShellNoCC { buildInputs = [ go gopls goimports-reviser ]; };
+              go = mkShell { buildInputs = [ go gopls goimports-reviser ]; };
               ocaml = mkShell {
                 nativeBuildInputs = with ocamlPackages; [ ocaml findlib dune_3 ocaml-lsp utop ocamlformat odoc ];
                 buildInputs = with ocamlPackages; [ ocamlgraph ocamlPackages.core ocamlPackages.core_unix ];
