87f23cd536c442047884192bd6ab5dc1f6d0085b
Fergus Molloy <fergus@molloy.xyz>
2025-08-09 14:34:24 +0100

    update hyprland config for multiple monitors

--- hosts/barioth/home.nix
+++ hosts/barioth/home.nix
@@ -5,8 +5,10 @@
     enable = true;
     enableLocking = true;
     extraConfig = ''
+      $mon1 = DP-1
+      $mon2 = HDMI-A-2
       monitor=DP-1,2560x1440@144,auto,auto
-      monitor=HDMI-A-1,1920x1080@60,-1920x0,auto
+      monitor=$mon2,1920x1080@60,-1920x0,auto
     '';
   };
 }
