0e6fd16fc627baba63f30fc2f1dbe0a0a2c16cba
Fergus Molloy <fergus@molloy.xyz>
2025-11-19 09:58:32 +0000

    add extra nameserver

--- hosts/common/networking.nix
+++ hosts/common/networking.nix
@@ -8,5 +8,8 @@
   networking = {
     firewall.enable = lib.mkDefault true;
     networkmanager.enable = lib.mkDefault true;
+    resolveconf.extraConfig = ''
+      nameserver 8.8.8.8
+    '';
   };
 }
